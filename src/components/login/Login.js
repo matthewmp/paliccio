e0547fada26d1965b543a22c89f02c0ddf430b95
matthewmp <matt.m.palumbo@gmail.com>
2019-04-14 21:14:06 -0400

    finished styles for login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,27 +2,54 @@ import React from "react";
 
 import "./login.css";
 class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.username = React.createRef();
+    this.password = React.createRef();
+  }
+  state = {
+    show: false,
+    username: '',
+    password: ''
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      username: this.username.current.value,
+      password: this.password.current.value
+    })
+  }
+
   render() {
     return (
       <div className="login-wrapper">
-        <div className="login-X-close">X</div>
+        <div className="login-X-close">&times;</div>
         <form id="login-form">
-          <div class="login-input-wrapper">
-            <label className="inp-label" htmlFor="inpUserName">Username</label>
-            <input
-              id="inpUserName"
-              type="text"
-              placeholder="Username"
-              required
-            />
+          <div className="login-input-wrapper">
+            <div className="inp-username-wrapper">
+              <label className="inp-label" htmlFor="inpUserName" >Username</label>
+              <input
+                id="inpUserName"
+                type="text"
+                ref={this.username} 
+                value={this.state.username} 
+                onChange={this.handleChange}
+                required
+              />
+            </div>
 
-            <label className="inp-label" htmlFor="inpUserPassword">Password</label>
-            <input
-              id="inpUserPassword"
-              type="text"
-              placeholder="Password"
-              required
-            />
+            <div className="inp-password-wrapper">
+              <label className="inp-label" htmlFor="inpUserPassword" >Password</label>
+              <input
+                id="inpUserPassword"
+                type="text"
+                ref={this.password} 
+                value={this.state.password} 
+                onChange={this.handleChange}
+                required
+              />
+            </div>    
           </div>
         </form>
       </div>
