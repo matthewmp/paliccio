cc671697c9ef8e9aa438fd8c259efc2de19c6ff2
matthewmp <matt.m.palumbo@gmail.com>
2019-06-09 09:44:50 -0400

    added modal for adding recipe

--- src/components/add_modify_modal/AddGeneralInfo.js
+++ src/components/add_modify_modal/AddGeneralInfo.js
@@ -5,19 +5,23 @@ export default class AddGeneralInfo extends Component {
     render() {
         return (
             <section className="add-edit-recipe-view general-info">
-                <div className="add-edit-recipe-label">General Info</div>
-                <input className="add-edit-recipe-name" 
-                    type="text" 
-                    placeholder="RECIPE NAME" 
-                    onChange={this.props.handleNameChange} 
-                    value={this.props.nameVal}/>
-                <textarea name="recipe-description" 
-                    id="recipe-description" 
-                    cols="30" 
-                    rows="60" 
-                    placeholder="Description..."
-                    onChange={this.props.handleDescriptionChange}
-                    value={this.props.descriptionVal}></textarea>
+                <div className="add-edit-recipe-label">Recipe General Info</div>
+                <div className="add-recipe-inputs-wrapper">
+                    <input className="add-edit-recipe-name" 
+                        type="text" 
+                        placeholder="RECIPE NAME" 
+                        onChange={this.props.handleNameChange} 
+                        value={this.props.nameVal}/>
+                    <textarea name="recipe-description" 
+                        id="recipe-description" 
+                        cols="30" 
+                        rows="60" 
+                        placeholder="Description..."
+                        onChange={this.props.handleDescriptionChange}
+                        value={this.props.descriptionVal}>
+                    </textarea>
+                    <button className="add-recipe-next" type="button">Next</button>
+                </div>
             </section>
         )
     }
