0d39285a5f07511d3041253ebd696c5d85efdb45
matthewmp <mmpal78@yahoo.com>
2019-08-24 08:12:54 -0400

    setup node env variables, installed morgan and dotenv

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
+// Get access to environment variables
+dotenv.config();
+// Setup morgan comments
+app.use(morgan('common'));
+
+// Serve up static files
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-    
-// })
+
 
 app.listen(3000, function(req, res){
     console.log('Running on Port 3000');
